b86cd31df8e98bf4e74935e3dac58842ae26a92b
EmileESTACA <61012998+EmileESTACA@users.noreply.github.com>
2020-03-03 17:40:10 +0100

    sous prog choix de la couleur

--- main.c
+++ main.c
@@ -6,9 +6,58 @@ Hugo Allaire
 #include<stdlib.h>
 #include<stdio.h>
 #include<windows.h>
+#include <time.h>
+
+void selectionCouleurs(int* player1,int* player2);
 
 int main(){
 
 
+
 }
-void
+
+void selectionCouleurs(int* player1,int* player2){
+   int age1;
+    int age2;
+    srand(time(NULL));
+    printf("quel est votre age joueur numero 1?\n");
+    scanf("%d",&age1);
+    system("clear");
+    printf("quel est votre age joueur numero 2");
+    scanf("%d",&age2);
+
+    if (age1<age2){
+        *player1=COULEUR_JAUNE;
+        *player2=COULEUR_VERTE;
+    }else{
+        if (age1>age2){
+            *player1=COULEUR_VERTE ;
+            *player2=COULEUR_JAUNE ;
+        }else{
+            int aléatoire = rand(%)2;
+            if (aléatoire=0){
+                *player1=COULEUR_JAUNE;
+                *player2=COULEUR_VERTE;
+            }else{
+            *player1=COULEUR_VERTE;
+            *player2=COULEUR_JAUNE;
+            }
+             
+            
+            
+            
+        }
+       
+        
+        
+    }    
+        
+        
+        
+    }
+
+
+
+
+
+}
