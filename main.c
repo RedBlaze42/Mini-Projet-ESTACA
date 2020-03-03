eef9e79673ad7e78d9cadb8d95a4bf30da70ba75
Redblaze42 <allairehugo@gmail.com>
2020-03-03 20:40:34 +0100

    Ajout des couleurs

--- main.c
+++ main.c
@@ -5,5 +5,40 @@ Hugo Allaire
 */
 #include<stdlib.h>
 #include<stdio.h>
+#include<windows.h>//Uniquement pour windows
 #define enclosPlayer1 0
 #define enclosPlayer2 17
+
+void color (int couleurDuTexte, int couleurDuFond){//Version windows
+    HANDLE H = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(H, couleurDuFond*16+couleurDuTexte);
+}
+/*
+void color (int couleurDuTexte, int couleurDuFond){//Version linux
+    if(couleurDuTexte==14){
+        printf("\e[38;5;226m");
+    }else if(couleurDuTexte==10){
+        printf("\e[38;5;118m");
+    }else if(couleurDuTexte==0){
+        printf("\e[38;5;232m");
+    }else if(couleurDuTexte==15){
+        printf("\e[38;5;255m");
+    }else if(couleurDuTexte==7){
+        printf("\e[38;5;250m");
+    }else if(couleurDuTexte==5){
+        printf("\e[38;5;201m");
+    }
+    if(couleurDuFond==14){
+        printf("\e[48;5;226m");
+    }else if(couleurDuFond==10){
+        printf("\e[48;5;118m");
+    }else if(couleurDuFond==0){
+        printf("\e[48;5;232m");
+    }else if(couleurDuFond==15){
+        printf("\e[48;5;255m");
+    }else if(couleurDuFond==7){
+        printf("\e[48;5;250m");
+    }else if(couleurDuFond==5){
+        printf("\e[48;5;201m");
+    }
+}*/
